9f6846b70136a4cc83d740dcf5ece49d8656e079
chenhuijun <642342217@qq.com>
2022-05-22 11:32:06 +0800

    fix bug

--- app.js
+++ app.js
@@ -21,16 +21,17 @@ app.use(function(req, res, next) {
 
 //读取文件，并存入数据库
 parse();
+parse();
 
-//获取所有文章信息
+//获取所有文章信息路由设置
 const articleRouter = require('./routers/allArticle');
 app.use('/', articleRouter);
 
-//获取目录列表
+//获取目录列表路由设置
 const categoryRouter = require('./routers/category');
 app.use('/', categoryRouter);
 
-//获取目录下相关文章信息
+//获取目录下相关文章信息路由设置
 const relArticleRouter = require('./routers/relatedArticle');
 app.use('/', relArticleRouter);
 
